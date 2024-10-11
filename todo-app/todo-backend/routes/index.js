a6eeb18e0ab04456d6056738eb5b402559263ad4
zntb <zenetibi@yahoo.com>
2024-10-11 09:26:54 +0300

    Add finished exercise 12.9

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const redis = require('../redis');
 
-const configs = require('../util/config')
+const configs = require('../util/config');
 
-let visits = 0
+let visits = 0;
 
-/* GET index data. */
 router.get('/', async (req, res) => {
-  visits++
+  let visits = await redis.getAsync('visits');
+  visits = visits ? parseInt(visits) : 0;
+
+  await redis.setAsync('visits', visits + 1);
 
   res.send({
     ...configs,
-    visits
+    visits: visits + 1,
+  });
+});
+
+router.get('/redis', async (req, res) => {
+  const value = await redis.getAsync('visits');
+  res.send({
+    visits: value ? parseInt(value) : 0,
   });
 });
 
