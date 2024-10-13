dad2a9a12c71e158cfd62a012e36e52a7c10de30
zntb <zenetibi@yahoo.com>
2024-10-13 11:19:11 +0300

    Add finished exercise 12.13

--- todo-app/todo-frontend/src/Todos/Form.jsx
+++ todo-app/todo-frontend/src/Todos/Form.jsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react'
+import { useState } from 'react';
 
 const TodoForm = ({ createTodo }) => {
-  const [text, setText] = useState('')
+  const [text, setText] = useState('');
 
   const onChange = ({ target }) => {
-    setText(target.value)
-  }
+    setText(target.value);
+  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    createTodo({ text })
-  }
+  const handleSubmit = e => {
+    e.preventDefault();
+    createTodo({ text });
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="text" value={text} onChange={onChange} />
-      <button type="submit"> Submit </button>
+      <input type='text' name='text' value={text} onChange={onChange} />
+      <button type='submit'> Submit </button>
     </form>
-  )
-}
+  );
+};
 
-export default TodoForm
+export default TodoForm;
